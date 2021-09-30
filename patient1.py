7d4cec5d48d20821eca6e5db7a3b49f43d890512
tucke430 <tucke430@umn.edu>
2021-09-30 15:17:52 -0500

    Ran patient 1 with all models

--- patient1.py
+++ patient1.py
@@ -270,7 +270,7 @@ K = 4
 skip = 0 
 
 b_size = 1
-epochs = 5
+epochs = 20
 
 tower1Shapes = tower2Shapes = [H, H, K]
 tower1Activators = tower2Activators = ['sigmoid', 'sigmoid', None]
@@ -329,7 +329,7 @@ D = lag+1
 
 
 b_size = 1
-epochs = 5
+epochs = 20
 
 lPat.resetData()
 rPat.resetData()
