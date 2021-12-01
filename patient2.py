c701e389eb9d7cb783dcd540cf9ec75863ac285c
tucke430 <tucke430@umn.edu>
2021-12-01 15:06:47 -0600

    Added timing

--- patient2.py
+++ patient2.py
@@ -10,6 +10,7 @@ Patient 2 analysis file
 import tensorflow as tf
 import numpy as np
 import pickle
+import time
 
 import patient as pat
 import customLayers as cLayers
@@ -87,6 +88,8 @@ model.compile(optimizer= 'SGD', #tf.keras.optimizers.SGD(learning_rate=0.0001)
 
 models["JDST"] = model
 
+ticJDST = time.perf_counter()
+
 trn.cvTraining(lPat,
                rPat,
                K,
@@ -99,6 +102,10 @@ trn.cvTraining(lPat,
                models,
                "JDST",
                callbacks)
+
+tocJDST = time.perf_counter()
+
+print(tocJDST - ticJDST)
 
 print("JDST Done")
 # %% Sequential w/2 Hidden Layers
@@ -475,6 +482,8 @@ model.compile(optimizer= 'SGD', #tf.keras.optimizers.SGD(learning_rate=0.0001)
               metrics=tf.keras.metrics.RootMeanSquaredError())
 models["GRU H=1"] = model
 
+ticGRU = time.perf_counter()
+
 trn.cvTraining(lPat,
                 rPat,
                 4,
@@ -488,6 +497,9 @@ trn.cvTraining(lPat,
                 "GRU H=1",
                 callbacks)
 
+tocGRU = time.perf_counter()
+
+print(tocGRU - ticGRU)
 print("GRU H=1 Done")
 
 # %% Save Results
